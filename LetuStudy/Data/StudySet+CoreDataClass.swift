5154c3512211dc3155353348e39912de461ae42a
richardhoman99 <rhoman99@icloud.com>
2023-04-05 16:19:28 -0500

    Fix Xcode's recognition of CoreData specific files

--- LetuStudy/Data/StudySet+CoreDataClass.swift
+++ LetuStudy/Data/StudySet+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  StudySet+CoreDataClass.swift
 //  LetuStudy
 //
-//  Created by Richard Homan on 3/29/23.
+//  Created by Richard Homan on 3/30/23.
 //
 //
 
@@ -10,9 +10,7 @@ import Foundation
 import CoreData
 
 @objc(StudySet)
-public class StudySet: NSManagedObject {
-	public var wrappedName: String
-	{
-		name ?? "Test Set"
-	}
+public class StudySet: NSManagedObject
+{
+
 }
