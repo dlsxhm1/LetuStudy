9359370d16fd6b4bf7b7a2329c738ea58c4dc032
Haoming Xu <dlsxhm@gmail.com>
2023-04-19 10:09:59 -0700

    Adding working StudycardView.swift

---
+++ LetuStudy/Views/StudycardView.swift
@@ -0,0 +1,91 @@
+//
+//  StudycardView.swift
+//  LetuStudy
+//
+//  Created by user237980 on 4/19/23.
+//
+
+import SwiftUI
+import CoreData
+
+struct StudycardView: View {
+	@ObservedObject private var keyboardManager = KeyboardManager()
+	
+	enum Focusable: Hashable
+	{
+		case none
+		case add
+		case row(id: String)
+	}
+	
+	@FocusState var focusedPoint: Focusable?
+	@State private var isEditing = false
+	@State private var points: [Point] = []
+
+	
+	var persistentStore: NSPersistentContainer =
+	{
+		let appDelegate = UIApplication.shared.delegate as! AppDelegate
+		return appDelegate.persistentContainer
+	}()
+	
+	var studysets: [Studyset] = [.init(name: "StudySet 1"), .init(name: "StudySet 2"), .init(name: "StudySet 3"),]
+	
+    var body: some View {
+		NavigationStack{
+			List{
+				ForEach(Studyset, id: \.name) { StudySet in
+					NavigationLink(destination: <#T##Destination#>, label: <#T##() -> Label#>)
+				}
+			}
+			.navigationTitle("Study Sets")
+			.navigationBarTitleDisplayMode(.large)
+			.toolbar
+			{
+				ToolbarItem(placement: .navigationBarTrailing)
+				{
+					Button(action:
+					{
+						guard !keyboardManager.isVisible else
+						{
+							focusedPoint = nil
+							return
+						}
+						withAnimation
+						{
+							self.isEditing = !isEditing
+						}
+					})
+					{
+						if (isEditing)
+						{
+							Text("Done")
+								.bold()
+						}
+						else
+						{
+							Text("Edit")
+						}
+					}
+					.transaction
+					{ t in
+						t.animation = .none
+					}
+					.disabled(!keyboardManager.isVisible && points.count <= 0)
+				}
+			}
+		}
+		
+    }
+}
+
+struct StudycardView_Previews: PreviewProvider {
+    static var previews: some View {
+        StudycardView()
+    }
+}
+
+struct Studyset: Identifiable, Hashable {
+	let id: String = UUID().uuidString
+	let name: String
+}
